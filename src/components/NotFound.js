f2cf831d274e7469b66608cffd1f62ea5b6c6640
Ignacio Saporiti <ignacio.saporiti@erni-espana.es>
2020-03-26 10:17:46 +0100

    Removed back button from NotFound component.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { useHistory } from 'react-router-dom';
 
 
 function NotFound() {
-    const history = useHistory();
-
-    return (
-        <>
-            <span>The resource you're looking for was not found!</span>
-            <Button onClick={history.goBack}>Go back</Button>
-        </>
-    )
+    return <span>The resource you're looking for was not found!</span>
 }
 
 export default NotFound;

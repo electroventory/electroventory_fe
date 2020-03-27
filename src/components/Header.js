553cd883fccd545c7bcb91f601b6545dba375cb4
yeyeto2788 <juanernestobiondi@gmail.com>
2020-03-27 11:12:17 +0100

    Fix minor issue on import.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/NavBar';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
 const tabs = [

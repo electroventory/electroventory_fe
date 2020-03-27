13fdaccc6328f6bc3af84b4ab55ab75ec7f1dbdf
Ignacio Saporiti <ignacio.saporiti@erni-espana.es>
2020-03-27 16:23:41 +0100

    Generic Form component: first iteration.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,42 +1,41 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from './Form';
 
-function handleSubmit(event) {
+function onSubmitCallback(event) {
     event.preventDefault();
     console.log("POST /users");
+};
+const schema = {
+    username: {
+        controlId: "username",
+        label: "Username",
+        inputType: "text"
+    },
+    email: {
+        controlId: "email",
+        label: "Email address",
+        inputType: "email"
+    },
+    password: {
+        controlId: "password",
+        label: "Enter a password",
+        inputType: "password"
+    },
+    passwordConfirmation: {
+        controlId: "passwordConfirmation",
+        label: "Re-enter your password",
+        inputType: "password"
+    }
 };
 
 function SignUp() {
 
     return (
-        <>
-            <h5>Create a new user</h5>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="username" />
-                    <Form.Text className="text-muted" />
-                </Form.Group>
-                <Form.Group controlId="email">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email"/>
-                    <Form.Text className="text-muted" />
-                </Form.Group>
-                <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" />
-                    <Form.Text className="text-muted" />
-                </Form.Group>
-                <Form.Group controlId="passwordConfirmation">
-                    <Form.Label>Confirm your password</Form.Label>
-                    <Form.Control type="password"/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Sign up
-                </Button>
-            </Form>
-        </>
+        <Form
+            onSubmitCallback={onSubmitCallback}
+            schema={schema}
+            submitText="Sign up"
+        />
     )
 }
 
